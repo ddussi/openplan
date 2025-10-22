9d5f7db8fc8d8d923d5bdd7bd32d506ada888578
ddussi <akfn0919@naver.com>
2025-10-22 22:06:37 +0900

    Feat: shared 패키지 export 설정

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,16 +1,13 @@
-// Stores (구현 예정)
-// export { usePhotoStore } from './stores/photoStore';
-// export type { PhotoData, PhotoState } from './stores/photoStore';
+// Stores
+export { usePhotoStore, selectPhoto, selectHasViewed, selectLastViewedAt } from './stores/photoStore';
+export type { PhotoData, PhotoState } from './stores/photoStore';
 
-// Services (구현 예정)
-// export { photoService } from './services/photoService';
-// export { usePhotoQuery } from './services/photoService';
+// Services
+export { photoService, usePhotoQuery } from './services/photoService';
 
-// Hooks (구현 예정)
-// export { useDebounce } from './hooks/useDebounce';
-// export { useThrottle } from './hooks/useThrottle';
+// Hooks
+export { useDebounce } from './hooks/useDebounce';
+export { useThrottle } from './hooks/useThrottle';
 
-// Utils (구현 예정)
-// export { storageUtils } from './utils/storage';
-
-export {};
+// Utils
+export { storageUtils } from './utils/storage';
